547640248774de9c7a1cbea69f555b70d2a133c5
lzhang17 <lzmunch@gmail.com>
2019-08-03 01:42:53 -0400

    initial commit nothing makes sense

---
+++ script.js
@@ -0,0 +1,291 @@
+var map;
+var BOUNDS = {
+  north: -34.36,
+  south: -47.35,
+  west: 166.28,
+  east: -175.81,
+};
+var CENTER = {lat: -37.06, lng: 174.58};
+var MARKERS = [];
+var SOUND_HOWLS = [];
+var NUM_SOUNDS = 0;
+var soundsLoaded;
+
+var sound1, marker1;
+
+var PATH_TO_SOUNDS = "sounds/";
+//SOUNDS_INFO
+
+//Functions
+var updateSounds;
+
+var move = [0,0];
+var zoom;
+//var keyCodes = {up: 87, down: 83, left: 65, right: 68, zoomIn:90, zoomOut:88};
+//var keyCodes = [87, 83, 65, 68, 90, 88];
+var keyCodes = [38, 40, 37, 39,90, 88];
+var keyDown = [false,false,false,false,false,false];
+var panAmt = 15;
+var fps = 60;
+
+
+function animateMap(){
+  setTimeout(function(){
+    //    console.log(move, keyDown);
+//    map.setZoom(zoom);
+    if (move[0] != 0 || move[1] != 0)
+      map.panBy(move[0], move[1]);
+    requestAnimationFrame(animateMap);  
+  }, 1000 / fps)
+
+}
+
+$(function(){
+  //  window.requestAnimationFrame(function(){map.panBy(move[0],move[1]);});
+
+  $(document).bind("keydown",function(e){
+    e.preventDefault();
+    //  alert(e.keyCode);
+    var kc = e.keyCode;
+    //  console.log(kc, keyCodes.up);
+    if (kc == keyCodes[0]){ //up
+      move[1] = -panAmt;
+      keyDown[0] = true;
+    }
+    if (kc == keyCodes[1]){ //down
+      move[1] = panAmt;
+      keyDown[1] = true;
+    }
+    if (kc == keyCodes[2]){ //left
+      move[0] = -panAmt;
+      keyDown[2] = true;
+    }
+    if (kc == keyCodes[3]){ //right
+      move[0] = panAmt;
+      keyDown[3] = true;
+    }
+
+    if (kc == keyCodes[4]){
+      map.setZoom(map.getZoom() + 1);
+//      zoom += 1;
+    }
+    else if (kc == keyCodes[5]){
+      map.setZoom(map.getZoom() - 1);
+//      zoom -= 1;
+    }
+    //    map.panBy(0,-panAmt);
+
+    //    console.log("keydown, move",move);
+  })
+
+  $(document).bind("keyup",function(e){
+    e.preventDefault();
+    //  alert(e.keyCode);
+    var kc = e.keyCode;
+
+    if (kc == keyCodes[0]){ //up
+      move[1] = keyDown[1] ? panAmt : 0;
+      keyDown[0] = false;
+    }
+    if (kc == keyCodes[1]){ //down
+      move[1] = keyDown[0] ? -panAmt : 0;
+      keyDown[1] = false;
+    }
+    if (kc == keyCodes[2]){ //left
+      move[0] = keyDown[3] ? panAmt : 0;
+      keyDown[2] = false;
+    }
+    if (kc == keyCodes[3]){ //right
+      move[0] = keyDown[2] ? -panAmt : 0;
+      keyDown[3] = false;
+    }
+  });
+
+  window.setInterval(function(){
+    //    console.log(move);
+    //    console.log("pan");
+    //    map.panBy(20,20)
+    //    map.panBy(move[0],move[1])
+    //        map.panBy(move[0],move[1]);
+  }, 50);
+
+  animateMap();
+
+});
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: CENTER,
+    restriction:{
+      latLngBounds: BOUNDS,
+      strictBounds: false,
+    },
+    zoom: 8,
+    disableDefaultUI: true
+  });
+  
+//  zoom = map.getZoom();
+
+  initSoundMarkers();
+  for (var i=0; i < MARKERS.length; i++){
+    new google.maps.Marker(MARKERS[i]);
+  }
+
+  //    initSounds(); 
+
+  //  initialize(); //easing??
+
+
+  //  marker1 = new google.maps.Marker( {position: CENTER, map: map, title: 'aukland', play: false},);
+  //  sound1 = new Howl({
+  //    src: ['Adventure Time Ending Theme.mp3'],
+  //    html5: true,
+  //    autoplay: false,
+  //  });
+  //  sound1.on('load', function(id) { console.log('loaded:', id);});   
+  //  sound1.on('play', function(id) {console.log('played:', id);});   
+  //  sound1.on('end', function(id) { console.log('ended:', id);});   
+
+  google.maps.event.addListenerOnce(map, 'idle', function () {
+    // map is ready
+    setTimeout(function() {
+      //      console.assert(NUM_SOUNDS == SOUND_HOWLS.length && NUM_SOUNDS == MARKERS.length);
+      while(NUM_SOUNDS != SOUND_HOWLS.length){}
+
+      google.maps.event.addListener(map, 'bounds_changed', function() {
+        var bounds =  map.getBounds();
+        var ne = bounds.getNorthEast();
+        var sw = bounds.getSouthWest();
+        updateSounds(bounds);
+      });
+
+    }, 2000);
+  });
+
+  //  google.maps.event.addListener(map, 'mousemove', function (event) {
+  //    //    displayCoordinates(event.latLng);
+  //    //          map.setCenter(event.latLng);
+  //
+  //  });
+
+}
+
+
+
+function initSoundMarkers(){
+  for (var i=0; i< SOUNDS_INFO.length; i++){
+    var s = SOUNDS_INFO[i];
+    console.log(s.filename);
+    //    createSoundMarker(s.filename, s.pos, s.descrip);
+    var m = {
+      position: s.pos,
+      map: map,
+      title: s.descrip,
+      play: false
+    };
+    var h = new Howl({
+      src: [PATH_TO_SOUNDS + s.filename],
+      html5: true,
+      autoplay: false,
+      pool: 1,
+    });
+
+    h.on('load', function(id) {
+      console.log('loaded:', id);
+      NUM_SOUNDS += 1;
+    });   
+    h.on('play', function(id) {
+      console.log('played:', id);
+    });   
+    h.on('end', function(id) {
+      console.log('ended:', id);
+    });             
+
+    SOUND_HOWLS.push(h);
+    MARKERS.push(m);
+  }
+}
+
+function updateSounds(bounds){
+  //  console.log(MARKERS[0])
+  for (var i=0; i < MARKERS.length; i++){
+    var pos = MARKERS[i].position;
+    var ne = bounds.ga;
+    var sw = bounds.na;
+    MARKERS[i].play = (ne.j < pos.lng) && (pos.lng < ne.l) && (sw.j < pos.lat) && (pos.lat < sw.l);
+  }
+
+  for (var i=0; i < MARKERS.length; i++){
+    var m = MARKERS[i];
+    var s = SOUND_HOWLS[i]
+    if (m.play && !s.playing()){
+      //      console.log("play")
+      s.play();
+    }
+    else if (!m.play && s.playing()){
+      //      console.log("stop")
+      //    sound1.mute(true);
+      s.pause();
+    }
+  }
+}
+
+function displayCoordinates(pnt) {
+  var lat = pnt.lat();
+  lat = lat.toFixed(4);
+  var lng = pnt.lng();
+  lng = lng.toFixed(4);
+  console.log("Latitude: " + lat + "  Longitude: " + lng);
+}
+
+//easing
+var EasingAnimator = function(opt){
+  opt = opt || {};
+  this.easingInterval = opt.easingInterval;
+  this.duration = opt.duration || 1000;
+  this.step = opt.step || 50;
+  this.easingFn = opt.easingFn  || function easeInOutElastic(t, b, c, d) {
+    if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+    return -c/2 * ((t-=2)*t*t*t - 2) + b;
+  };
+  this.callBack = opt.callBack || function(){};
+};
+
+EasingAnimator.makeFromCallback = function(callBack){
+  return new EasingAnimator({
+    callBack: callBack
+  });
+};
+
+EasingAnimator.prototype.easeProp = function(obj, propDict){
+  propDict = propDict || {};
+
+  var self = this,
+      t = 0,
+      out_vals = JSON.parse(JSON.stringify(obj));
+
+  clearInterval(self.easingInterval);
+  self.easingInterval = setInterval(function(){
+    t+= self.step;
+    if (t >= self.duration) {
+      clearInterval(self.easingInterval);
+      self.callBack(propDict);
+      return;
+    }
+    var percent = self.easingFn(t, 0, 1, self.duration);
+    Object.keys(propDict).forEach(function(key, i) {
+      var old_val = obj[key];
+
+      out_vals[key] = old_val - percent*(old_val - propDict[key]);
+    });
+    self.callBack(out_vals);
+  }, self.step);
+};
+
+function initialize() {
+  var easingAnimator = EasingAnimator.makeFromCallback(function(latLng){
+    map.setCenter(latLng) 
+  })
+  }
+
+//google.maps.event.addDomListener(window, 'load', initialize);
---
+++ sounds-info.js
@@ -0,0 +1,17 @@
+var SOUNDS_INFO = [
+  {
+    filename: "Footage of The Troubles in Ireland.mp3",
+    pos: {lat: -36.3714, lng: 175.1609},
+    descrip: "Footage of The Troubles in Ireland.mp3"
+  },
+  {
+    filename: "Adventure Time Ending Theme.mp3",
+    pos: {lat: -37.00, lng: 173.58},
+    descrip: "Adventure Time Ending Theme.mp3"
+  },
+  {
+    filename: "NolanLive - Irish Language Act.mp3",
+    pos: {lat: -37.06, lng: 174.58},
+    descrip: "NolanLive - Irish Language Act.mp3"
+  }
+];
