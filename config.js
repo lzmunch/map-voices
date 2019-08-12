9da8c64495c45cc2c56ccce9c1e8d4bfff56c769
Lauren Zhang <lauren.zll.zhang@gmail.com>
2019-08-12 15:12:03 -0400

    moved api key

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+var API_KEY = "AIzaSyAxCvGaKe4Z_q3zWNy-GA10JVizY7zCeH0";
+
+var js_file = document.createElement('script');
+js_file.type = 'text/javascript';
+js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&key=' + API_KEY;
+js_file.async = 'sync';
+js_file.defer = 'defer';
+(document.getElementsByTagName('head'))[0].appendChild(js_file);
